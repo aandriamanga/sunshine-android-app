d32e3a65ebc2525eb63290c463b7559943d4cf61
Unknown <a.tojo.antonio@gmail.com>
2018-07-31 01:30:53 +0200

    configure  GitHub CI: App Center

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,11 +17,17 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        AppCenter.start(getApplication(), "058a88f4-8ac1-42f6-bc51-1f9e839b4aee", Analytics.class, Crashes.class);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
     }
