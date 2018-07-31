278503591bfc79981de2634f409c4ef410bd2e98
Unknown <a.tojo.antonio@gmail.com>
2018-08-01 01:04:06 +0200

    Init adapter & ViewHolder

---
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,13 @@
+package com.example.android.sunshine;
+
+public class ForecastAdapter {
+    // TODO (22) Extend RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder>
+
+    // TODO (23) Create a private string array called mWeatherData
+
+    // TODO (47) Create the default constructor (we will pass in parameters in a later lesson)
+
+    // TODO (16) Create a class within ForecastAdapter called ForecastAdapterViewHolder
+    // TODO (17) Extend RecyclerView.ViewHolder
+
+}
