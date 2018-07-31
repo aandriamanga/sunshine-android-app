c1a451447ea4afec53b8d62a3f41f04969f14c78
Unknown <a.tojo.antonio@gmail.com>
2018-08-01 01:24:16 +0200

    create adapter

--- app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,5 +1,9 @@
 package com.example.android.sunshine;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
 public class ForecastAdapter {
     // TODO (22) Extend RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder>
 
@@ -7,7 +11,21 @@ public class ForecastAdapter {
 
     // TODO (47) Create the default constructor (we will pass in parameters in a later lesson)
 
-    // TODO (16) Create a class within ForecastAdapter called ForecastAdapterViewHolder
-    // TODO (17) Extend RecyclerView.ViewHolder
+    // COMPLETE (16) Create a class within ForecastAdapter called ForecastAdapterViewHolder
+    // COMPLETE (17) Extend RecyclerView.ViewHolder
+    class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+        // COMPLETE (18) Create a public final TextView variable called mWeatherTextView
+        public final TextView mWeatherTextView;
 
+
+        // COMPLETE (19) Create a constructor for this class that accepts a View as a parameter
+        public ForecastAdapterViewHolder(View itemView) {
+            // COMPLETE (20) Call super(view) within the constructor for ForecastAdapterViewHolder
+            super(itemView);
+
+            // COMPLETE (21) Using view.findViewById, get a reference to this layout's TextView and save it to mWeatherTextView
+            mWeatherTextView = (TextView) itemView.findViewById(R.id.tv_weather_data);
+
+        }
+    }
 }
--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -42,11 +42,7 @@ public class MainActivity extends AppCompatActivity {
     // TODO (15) Add a class file called ForecastAdapter
     // Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
 
-    // TODO (18) Create a public final TextView variable called mWeatherTextView
 
-    // TODO (19) Create a constructor for this class that accepts a View as a parameter
-    // TODO (20) Call super(view) within the constructor for ForecastAdapterViewHolder
-    // TODO (21) Using view.findViewById, get a reference to this layout's TextView and save it to mWeatherTextView
     // Within ForecastAdapterViewHolder ///////////////////////////////////////////////////////////
 
 
